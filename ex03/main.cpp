2d59460008a3d1480fe0255322f0dea928aeefc7
jikho7 <113023191+jikho7@users.noreply.github.com>
2026-01-13 03:03:05 +0100

    Update main.cpp

--- ex03/main.cpp
+++ ex03/main.cpp
@@ -1,33 +1,57 @@
 #include "ClapTrap.hpp"
 #include "ScavTrap.hpp"
 #include "FragTrap.hpp"
-#include "DiamondTrap.hpp"
 
 int main()
 {
-	ClapTrap clap;
-	ScavTrap scav(50);
-	FragTrap fra(20, 30);
-	DiamondTrap diamond("diamond", 10, 20);
+	ClapTrap plop("plop");
+	ClapTrap foo("foo");
+	ScavTrap Scav1("Scav1");
+	ScavTrap Scav2("Scav2");
+	FragTrap fra1("fra1");
+// 
+	std::cout << "\n";
 
-	diamond.whoAmI();
- 
-	scav.attack("zombie");
-	scav.beRepaired(10);
-	scav.takeDamage(5);
-	// std::cout << "Hit points: \033[1m" << plop.getHitPoints() << " \033[0mhit points." << std::endl;
-	scav.getHitPoints();
-	scav.attack("gobelin");
-	scav.beRepaired(10);
-	scav.takeDamage(155);
-	scav.guardGate();
+	plop.attack("zombie");
+	plop.beRepaired(10);
+	plop.takeDamage(5);
+	std::cout << "Plop : Hit points: \033[1m" << plop.getHitPoints() << " \033[0mhit points." << std::endl;
+	std::cout << "Foo : Hit points: \033[1m" << foo.getHitPoints() << " \033[0mhit points." << std::endl;
+	foo = plop;
+	std::cout << "Foo after cpy : Hit points: \033[1m" << foo.getHitPoints() << " \033[0mhit points." << std::endl;
 
-	fra.attack("rat");
-	fra.highFivesGuys();
-	fra.takeDamage(200);
-	fra.attack("gobelin");
-	scav.attack("rat");
-	scav.beRepaired(10);
+	std::cout << "\n";
+
+	Scav1.attack("human");
+	Scav1.beRepaired(10);
+	Scav1.takeDamage(5);
+	Scav1.guardGate();
+	std::cout << "scav1 : Hit points: \033[1m" << Scav1.getHitPoints() << " \033[0mhit points." << std::endl;
+	std::cout << "scav2 : Hit points: \033[1m" << Scav2.getHitPoints() << " \033[0mhit points." << std::endl;
+	Scav2 = Scav1;
+	std::cout << "Scav2 after cpy : Hit points: \033[1m" << Scav2.getHitPoints() << " \033[0mhit points." << std::endl;
+
+	Scav2.attack("gobelin");
+	Scav2.beRepaired(10);
+	Scav2.takeDamage(200);
+ std::cout << "Scav2 : hit points : " << Scav2.getHitPoints() << std::endl;
+
+	Scav1.attack("gobelin");
+	Scav1.takeDamage(100);
+	Scav1.beRepaired(20);
+ std::cout << "Scav1 : hit points : " << Scav1.getHitPoints() << std::endl;
+	std::cout << "Scav2 : hit points : " << Scav2.getHitPoints() << std::endl;
+
+ Scav1.guardGate();
+	Scav2.guardGate();
+	std::cout << "\n";
+
+	fra1.attack("rat");
+	fra1.highFivesGuys();
+	fra1.beRepaired(10);
+	// std::cout << "fra1 : Hit points: \033[1m" << fra1.getHitPoints() << " \033[0mhit points." << std::endl;
+	fra1.takeDamage(500);
+	std::cout << "\n";
+
 	return (0);
-
 }
