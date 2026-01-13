979fa2b3af608fb58c7c44d4ce07b61616b8c840
jikho7 <113023191+jikho7@users.noreply.github.com>
2026-01-13 02:39:14 +0100

    Update ScavTrap.hpp

--- ex03/ScavTrap.hpp
+++ ex03/ScavTrap.hpp
@@ -9,7 +9,7 @@ class ScavTrap : virtual public ClapTrap
 {
 	public:
 	ScavTrap();
-	ScavTrap(int energyPoints);
+	ScavTrap(std::string name);
 	~ScavTrap();
 	ScavTrap(const ScavTrap &other);
 	ScavTrap& operator=(const ScavTrap &other);
@@ -18,7 +18,7 @@ class ScavTrap : virtual public ClapTrap
 	void attack(const std::string& target);
 	
 	protected:
-	int _energyPoints;
+	
 };
 
 #endif
